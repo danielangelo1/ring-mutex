4aa542fe8e6085076dbdc98a7c18e611b3478b49
Daniel <danielangelo1234@gmail.com>
2024-02-05 14:50:40 -0300

    Refactor client.py and docker-compose.yml

--- start_election.py
+++ start_election.py
@@ -1,8 +1,11 @@
-# client.py
 
 from client_common import Client
 import sys
+import time
+
 
 client_id, port, next_port = map(int, sys.argv[1:4])
 client = Client(client_id, port, next_port)
 print(f"Cliente {client_id}, {port}, {next_port} criado.")
+time.sleep(5)
+client.start_election()
